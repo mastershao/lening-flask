d7d5e2289f2447927b0544a16e0366ec71289acd
邵文龙 <mastershao@163.com>
2016-04-13 11:08:17 +0800

    Signed-off-by: 邵文龙 <mastershao@163.com>

--- flask/run.py
+++ flask/run.py
@@ -6,5 +6,13 @@ app=Flask(__name__)
 def index():
 	return "<h1>test page<h1>"
 
+@app.route("/<username>")
+def show_user_profile(username):
+	return '<h1>use %s</h1>' %username
+
+@app.route("/<int:post_id>")
+def show_post(post_id):
+	return "<h1>post %d</h1>" %post_id
+
 app.debug=True
 app.run(host="0.0.0.0",port=80)
